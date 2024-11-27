6c0ea8e2a5d306750fb571d189c593aad3cf2efe
Guilherme Bastos <guiapbastos201881@gmail.com>
2024-11-27 16:26:09 -0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const faqQuestions = document.querySelectorAll(".faq__question");
+document.addEventListener("DOMContentLoaded", () => { //Garante que o programa só seja executado depois que o conteúdo HTML for carregado
+    const faqQuestions = document.querySelectorAll(".faq__question"); //Seleciona todos os elementos da página que possuem a classe .faq__question
 
-    faqQuestions.forEach((question) => {
-        question.addEventListener("click", () => {
-            const answer = question.nextElementSibling;
+    faqQuestions.forEach((question) => { //Adiciona lógica individualmente a cada pergunta
+        question.addEventListener("click", () => { //Quando o usuário clicar na pergunta, o código dentro da função será executado
+            const answer = question.nextElementSibling; //Usa o nextElementSibling para pegar o elemento logo abaixo da pergunta no DOM
 
             answer.classList.toggle("faq__answer--visible");
-
+            //Altera as classes CSS no elemento correspondente
             question.classList.toggle("faq__question--active");
         });
     });
 });
+
+// => substitui a palavra-chave function
 
